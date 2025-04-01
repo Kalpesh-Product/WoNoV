114c7e6f58a6a067a2f85e99713a198023fb0506
sankalp-wono <sankalp.wono@gmail.com>
2025-04-01 16:32:59 +0530

    changed axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://wono-admin-panel-be.vercel.app/",
+  baseURL: import.meta.env.VITE_PROD_LINK ,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: "https://wono-admin-panel-be.vercel.app/",
+  baseURL: import.meta.env.VITE_PROD_LINK ,
   withCredentials: true,
 });
 // export const api = axios.create({
