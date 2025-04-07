5abb470f1c2ce638df880aa60803bb95d7a51d0b
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2025-04-07 13:30:33 +0530

    Added date range selecter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "grapesjs/dist/css/grapes.min.css";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 import { SidebarProvider } from "./context/SideBarContext";
 import AuthContextProvider from "./context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
--- client/src/pages/Dashboard/AdminDashboard/AdminTeamMembersSchedule.jsx
+++ client/src/pages/Dashboard/AdminDashboard/AdminTeamMembersSchedule.jsx
@@ -3,29 +3,74 @@ import AgTable from "../../../components/AgTable";
 import PrimaryButton from "../../../components/PrimaryButton";
 import MuiModal from "../../../components/MuiModal";
 import { Controller, useForm } from "react-hook-form";
-import { Button, FormHelperText, MenuItem, TextField } from "@mui/material";
+import {
+  Button,
+  CircularProgress,
+  FormHelperText,
+  MenuItem,
+  TextField,
+} from "@mui/material";
 import { toast } from "sonner";
 import useAuth from "../../../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
-import { LocalizationProvider, StaticDatePicker } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { Calendar, DateRange } from "react-date-range";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
+
 const AdminTeamMembersSchedule = () => {
   const navigate = useNavigate();
+  const axios = useAxiosPrivate();
   const { auth } = useAuth();
   const [modalMode, setModalMode] = useState("add");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedAsset, setSelectedAsset] = useState(null);
+  const [selectionRange, setSelectionRange] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
   const {
     handleSubmit,
     control,
+    setValue,
     formState: { errors },
   } = useForm({
     defaultValues: {
-      name: "",
-      manager: "",
-      unit: "",
+      unitId: "",
+      startDate: new Date(),
+      endDate: new Date(),
     },
   });
+
+  //----------------------------------------API---------------------------------------//
+  const { data: unitsData = [], isPending: isUnitsPending } = useQuery({
+    queryKey: ["unitsData"],
+    queryFn: async () => {
+      try {
+        const response = await axios.get("/api/company/fetch-units");
+        console.log(response.data);
+        return response.data;
+      } catch (error) {
+        console.error("Error fetching clients data:", error);
+      }
+    },
+  });
+
+    const { data: employees, isLoading } = useQuery({
+      queryKey: ["employees"],
+      queryFn: async () => {
+        try {
+          const adminDepId = "6798bae6e469e809084e24a4"
+          const response = await axios.get(`/api/users/fetch-users/${adminDepId}`);
+          return response.data;
+        } catch (error) {
+          throw new Error(error.response.data.message);
+        }
+      },
+    });
+  //----------------------------------------API---------------------------------------//
+
   // Hardcoded data
   const teamMembers = [
     {
@@ -82,10 +127,23 @@ const AdminTeamMembersSchedule = () => {
     setIsModalOpen(true);
   };
   const handleFormSubmit = (data) => {
-    // Handle form submission logic here
+    console.log("Form data to send:", data); // includes startDate and endDate
     toast.success("Data submitted successfully!");
     setIsModalOpen(false);
   };
+
+  const handleDateSelect = (ranges) => {
+    const { startDate, endDate } = ranges.selection;
+    console.log("Start:", startDate);
+    console.log("End:", endDate);
+
+    setSelectionRange(ranges.selection);
+
+    // Update form state
+    setValue("startDate", startDate);
+    setValue("endDate", endDate);
+  };
+
   return (
     <div className="p-4">
       <AgTable
@@ -103,11 +161,44 @@ const AdminTeamMembersSchedule = () => {
           <div>
             <form onSubmit={handleSubmit(handleFormSubmit)}>
               <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4">
-                  <div className="col-span-2">
-                  <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <StaticDatePicker />
-                  </LocalizationProvider>
-                  </div>
+                <div className="col-span-2 w-full">
+                  <DateRange
+                    ranges={[selectionRange]}
+                    onChange={handleDateSelect}
+                    moveRangeOnFirstSelection={false}
+                  />
+                </div>
+                <div className="col-span-2">
+                  <Controller
+                    name="unitId"
+                    rules={{ required: "Unit is required" }}
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        label={"Select Unit"}
+                        size="small"
+                        fullWidth
+                        error={!!errors.unitId}
+                        helperText={errors.unitId?.message}
+                        select
+                      >
+                        <MenuItem value="" disabled>
+                          Select Unit
+                        </MenuItem>
+                        {!isUnitsPending ? (
+                          unitsData.map((item) => (
+                            <MenuItem key={item._id} value={item._id}>
+                              {item.unitNo}
+                            </MenuItem>
+                          ))
+                        ) : (
+                          <CircularProgress />
+                        )}
+                      </TextField>
+                    )}
+                  />
+                </div>
               </div>
               <div className="flex gap-4 justify-center items-center mt-4">
                 <PrimaryButton title="Submit" />
