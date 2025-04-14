da92536b5699bfa1293e94ec6e8931c34f3b3483
sankalp-wono <sankalp.wono@gmail.com>
2025-04-14 23:36:16 +0530

    changed settings page of sales

--- client/src/pages/Dashboard/SalesDashboard/SalesSettings/SalesPolicies.jsx
+++ client/src/pages/Dashboard/SalesDashboard/SalesSettings/SalesPolicies.jsx
@@ -2,9 +2,12 @@ import { IoMdDownload } from "react-icons/io";
 import { MdUpload } from "react-icons/md";
 import WidgetSection from "../../../../components/WidgetSection";
 import AgTable from "../../../../components/AgTable";
+import PrimaryButton from "../../../../components/PrimaryButton";
 
 const SalesPolicies = () => {
-  const uploadItems = ["Upload Policies"];
+  const uploadItems = [
+    "Upload Policies"
+  ];
   const policiesUploadDataColumns = [
     { field: "srNo", headerName: "Sr No", flex: 1 },
     { field: "templateName", headerName: "Template Name", flex: 1 },
@@ -15,69 +18,71 @@ const SalesPolicies = () => {
   const policiesUploadData = [
     {
       srNo: 1,
-      templateName: "Upload Policies",
-      uploadedBy: "John Doe",
-      date: "01-03-2025"
+      templateName: "Sales Code of Conduct Policy",
+      uploadedBy: "Kalpesh Naik",
+      date: "18-11-2024"
     },
     {
       srNo: 2,
-      templateName: "Upload Policies",
-      uploadedBy: "Jane Smith",
-      date: "08-03-2025"
+      templateName: "Client Engagement & Ethics Policy",
+      uploadedBy: "Kalpesh Naik",
+      date: "07-12-2024"
     },
     {
       srNo: 3,
-      templateName: "Upload Policies",
-      uploadedBy: "Michael Johnson",
-      date: "15-03-2025"
+      templateName: "Sales Team Anti-Harassment Policy",
+      uploadedBy: "Kalpesh Naik",
+      date: "19-01-2025"
     },
     {
       srNo: 4,
-      templateName: "Upload Policies",
-      uploadedBy: "Emily Davis",
-      date: "22-03-2025"
+      templateName: "Field Work & Remote Sales Policy",
+      uploadedBy: "Kalpesh Naik",
+      date: "11-03-2025"
     }
   ];
-  
-  
+
 
   return (
-    <div className="">
-      <h2 className="text-title font-pmedium text-primary pb-4">
-        Upload Policies
-      </h2>
+    <div className=" flex flex-col gap-4">
+      <span className="text-title font-pmedium text-primary">
+        Bulk Upload Data
+      </span>
+      <hr />
 
-      <div className="grid lg:grid-cols-3 md:grid-col-3 sm:grid-col-1 pb-4">
+      <div className="grid lg:grid-cols-3 md:grid-col-3 sm:grid-col-1">
         {uploadItems.map((index, item) => {
           return (
             <>
               <div
                 className="space-y-2 border-default p-4  rounded-md"
-                key={index}>
-                <div className="mb-2">{index}</div>
-                <div className="flex space-x-2">
+                key={index}
+              >
+                <div className="mb-2">
+                  <span className="text-subtitle text-primary">{index}</span>
+                </div>
+                <div className="flex gap-4">
                   {/* Placeholder Input Box */}
-                  <div className="flex items-end w-full border border-gray-200 rounded-md">
-                    <span className="text-white bg-gray-600 rounded-md p-2 ml-auto">
-                      Choose file
-                    </span>
+                  <div className="flex items-end w-full border justify-end border-gray-200 rounded-md">
+                    <PrimaryButton title={"Choose File"} />
                   </div>
-
-                  {/* Filter Button */}
-                  <button className="bg-[#48BBCC] p-2">
-                    <MdUpload style={{ fill: "white" }} />
-                  </button>
-                  <button className="bg-[#48BBCC] p-2">
-                    <IoMdDownload style={{ fill: "white" }} />
-                  </button>
+                  <div className="flex gap-2 items-center">
+                    <div className="bg-borderGray text-black p-2 rounded-md cursor-pointer hover:bg-gray-200 transition-all">
+                      <MdUpload style={{ fill: "black" }} />
+                    </div>
+                    <div className="bg-borderGray text-black p-2 rounded-md cursor-pointer hover:bg-gray-200 transition-all">
+                      <IoMdDownload style={{ fill: "black" }} />
+                    </div>
+                  </div>
                 </div>
               </div>
             </>
           );
         })}
       </div>
+
       <div>
-        <WidgetSection border title="Policies Data">
+        <WidgetSection border title="Bulk Upload Data">
           <AgTable
             data={policiesUploadData}
             columns={policiesUploadDataColumns}
--- client/src/pages/Dashboard/SalesDashboard/SalesSettings/SalesSops.jsx
+++ client/src/pages/Dashboard/SalesDashboard/SalesSettings/SalesSops.jsx
@@ -2,9 +2,12 @@ import { IoMdDownload } from "react-icons/io";
 import { MdUpload } from "react-icons/md";
 import WidgetSection from "../../../../components/WidgetSection";
 import AgTable from "../../../../components/AgTable";
+import PrimaryButton from "../../../../components/PrimaryButton";
 
 const SalesSops = () => {
-  const uploadItems = ["Upload Sops"];
+  const uploadItems = [
+    "Upload Sops"
+  ];
   const sopsUploadDataColumns = [
     { field: "srNo", headerName: "Sr No", flex: 1 },
     { field: "templateName", headerName: "Template Name", flex: 1 },
@@ -15,58 +18,64 @@ const SalesSops = () => {
   const sopsUploadData = [
     {
       srNo: 1,
-      templateName: "Upload Sops",
-      uploadedBy: "John Doe",
-      date: "01-03-2025"
+      templateName: "Sales Onboarding Process",
+      uploadedBy: "Kalpesh Naik",
+      date: "23-11-2024"
     },
     {
       srNo: 2,
-      templateName: "Upload Sops",
-      uploadedBy: "Jane Smith",
-      date: "08-03-2025"
+      templateName: "Lead Management SOP",
+      uploadedBy: "Kalpesh Naik",
+      date: "14-12-2024"
     },
     {
       srNo: 3,
-      templateName: "Upload Sops",
-      uploadedBy: "Michael Johnson",
-      date: "15-03-2025"
+      templateName: "Sales Performance Review Guidelines",
+      uploadedBy: "Kalpesh Naik",
+      date: "28-01-2025"
     },
     {
       srNo: 4,
-      templateName: "Upload Sops",
-      uploadedBy: "Emily Davis",
-      date: "22-03-2025"
+      templateName: "Client Handoff & Closure Procedure",
+      uploadedBy: "Kalpesh Naik",
+      date: "09-03-2025"
     }
   ];
-  
+
+
+
 
   return (
-    <div className="">
-      <h2 className="text-title font-pmedium text-primary pb-4">Upload Sops</h2>
+    <div className=" flex flex-col gap-4">
+      <span className="text-title font-pmedium text-primary">
+        Bulk Upload Data
+      </span>
+      <hr />
 
-      <div className="grid lg:grid-cols-3 md:grid-col-3 sm:grid-col-1 pb-4">
+      <div className="grid lg:grid-cols-3 md:grid-col-3 sm:grid-col-1">
         {uploadItems.map((index, item) => {
           return (
             <>
               <div
                 className="space-y-2 border-default p-4  rounded-md"
-                key={index}>
-                <div className="mb-2">{index}</div>
-                <div className="flex space-x-2">
+                key={index}
+              >
+                <div className="mb-2">
+                  <span className="text-subtitle text-primary">{index}</span>
+                </div>
+                <div className="flex gap-4">
                   {/* Placeholder Input Box */}
-                  <div className="flex items-end w-full border border-gray-200 rounded-md">
-                    <span className="text-white bg-gray-600 rounded-md p-2 ml-auto">
-                      Choose file
-                    </span>
+                  <div className="flex items-end w-full border justify-end border-gray-200 rounded-md">
+                    <PrimaryButton title={"Choose File"} />
                   </div>
-
-                  {/* Filter Button */}
-                  <button className="bg-[#48BBCC] p-2">
-                    <MdUpload style={{ fill: "white" }} />
-                  </button>
-                  <button className="bg-[#48BBCC] p-2">
-                    <IoMdDownload style={{ fill: "white" }} />
-                  </button>
+                  <div className="flex gap-2 items-center">
+                    <div className="bg-borderGray text-black p-2 rounded-md cursor-pointer hover:bg-gray-200 transition-all">
+                      <MdUpload style={{ fill: "black" }} />
+                    </div>
+                    <div className="bg-borderGray text-black p-2 rounded-md cursor-pointer hover:bg-gray-200 transition-all">
+                      <IoMdDownload style={{ fill: "black" }} />
+                    </div>
+                  </div>
                 </div>
               </div>
             </>
@@ -75,7 +84,7 @@ const SalesSops = () => {
       </div>
 
       <div>
-        <WidgetSection border title="SOPs Data">
+        <WidgetSection border title="Bulk Upload Data">
           <AgTable
             data={sopsUploadData}
             columns={sopsUploadDataColumns}
