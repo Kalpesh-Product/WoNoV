2fc4cd0b08a7b489745bdd4ea255f0d65077a464
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2025-04-15 01:12:50 +0530

    Sales completed

--- client/src/pages/Dashboard/SalesDashboard/SalesSettings/SalesSops.jsx
+++ client/src/pages/Dashboard/SalesDashboard/SalesSettings/SalesSops.jsx
@@ -3,7 +3,6 @@ import { MdUpload } from "react-icons/md";
 import WidgetSection from "../../../../components/WidgetSection";
 import AgTable from "../../../../components/AgTable";
 import PrimaryButton from "../../../../components/PrimaryButton";
-import PrimaryButton from "../../../../components/PrimaryButton";
 
 const Sops = () => {
   const uploadItems = ["Upload SOP"];
@@ -19,32 +18,25 @@ const Sops = () => {
       srNo: 1,
       templateName: "Upload SOP",
       uploadedBy: "Kalpesh Naik",
-      date: "01-03-2025"
+      date: "01-03-2025",
     },
     {
       srNo: 2,
       templateName: "Upload SOP",
       uploadedBy: "Kalpesh Naik",
-      date: "08-03-2025"
+      date: "08-03-2025",
     },
   ];
 
-
-
-
   return (
-    <div className=" flex flex-col gap-4">
-    <span className="text-title font-pmedium text-primary">
-      Upload SOP
-    </span>
-    <hr />
+    <div className="flex flex-col gap-4">
+      <h2 className="text-title font-pmedium text-primary pb-4">Upload Sops</h2>
 
-      <div className="grid lg:grid-cols-3 md:grid-col-3 sm:grid-col-1">
       <div className="grid lg:grid-cols-3 md:grid-col-3 sm:grid-col-1">
         {uploadItems.map((index, item) => {
           return (
             <>
-                <div
+              <div
                 className="space-y-2 border-default p-4  rounded-md"
                 key={index}
               >
@@ -52,23 +44,7 @@ const Sops = () => {
                   <span className="text-subtitle text-primary">{index}</span>
                 </div>
                 <div className="flex gap-4">
-                key={index}
-              >
-                <div className="mb-2">
-                  <span className="text-subtitle text-primary">{index}</span>
-                </div>
-                <div className="flex gap-4">
                   {/* Placeholder Input Box */}
-                  <div className="flex items-end w-full border justify-end border-gray-200 rounded-md">
-                    <PrimaryButton title={"Choose File"} />
-                  </div>
-                  <div className="flex gap-2 items-center">
-                    <div className="bg-borderGray text-black p-2 rounded-md cursor-pointer hover:bg-gray-200 transition-all">
-                      <MdUpload style={{ fill: "black" }} />
-                    </div>
-                    <div className="bg-borderGray text-black p-2 rounded-md cursor-pointer hover:bg-gray-200 transition-all">
-                      <IoMdDownload style={{ fill: "black" }} />
-                    </div>
                   <div className="flex items-end w-full border justify-end border-gray-200 rounded-md">
                     <PrimaryButton title={"Choose File"} />
                   </div>
