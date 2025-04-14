3489c6dca5b38f288de534d6c4e479150bad37cd
allanwono <allan.wono@gmail.com>
2025-04-14 18:21:56 +0530

    visitor reports view details button

--- client/src/pages/Visitors/VisitorReports.jsx
+++ client/src/pages/Visitors/VisitorReports.jsx
@@ -1,7 +1,13 @@
 import AgTable from "../../components/AgTable";
 import { Chip } from "@mui/material";
+import PrimaryButton from "../../components/PrimaryButton";
+import { useState } from "react";
 
 const VisitorReports = () => {
+  const [modalMode, setModalMode] = useState("add");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedVisitor, setSelectedVisitor] = useState([]);
+
   const meetingReportsColumn = [
     { field: "srNo", headerName: "Sr No" },
     { field: "name", headerName: "Name" },
@@ -12,34 +18,55 @@ const VisitorReports = () => {
     { field: "duration", headerName: "To Meet" },
     { field: "creditsUsed", headerName: "Check In" },
     { field: "checkOut", headerName: "Check Out" },
-    {
-      field: "status",
-      headerName: "Actions",
-      cellRenderer: (params) => {
-        const statusColorMap = {
-          "View Details": { backgroundColor: "#1e3d73", color: "#ffffff" }, // Light blue bg, dark blue font
-          Cancelled: { backgroundColor: "#f7e1e1", color: "#a5333e" }, // Light red bg, dark red font
-          Upcoming: { backgroundColor: "#fcf7be", color: "#b87e33" },
-        };
+    // {
+    //   field: "status",
+    //   headerName: "Actions",
+    //   cellRenderer: (params) => {
+    //     const statusColorMap = {
+    //       "View Details": { backgroundColor: "#1e3d73", color: "#ffffff" }, // Light blue bg, dark blue font
+    //       Cancelled: { backgroundColor: "#f7e1e1", color: "#a5333e" }, // Light red bg, dark red font
+    //       Upcoming: { backgroundColor: "#fcf7be", color: "#b87e33" },
+    //     };
 
-        const { backgroundColor, color } = statusColorMap[params.value] || {
-          backgroundColor: "gray",
-          color: "white",
-        };
-        return (
-          <>
-            <Chip
-              label={params.value}
-              style={{
-                backgroundColor,
-                color,
-              }}
-            />
-          </>
-        );
-      },
+    //     const { backgroundColor, color } = statusColorMap[params.value] || {
+    //       backgroundColor: "gray",
+    //       color: "white",
+    //     };
+    //     return (
+    //       <>
+    //         <Chip
+    //           label={params.value}
+    //           style={{
+    //             backgroundColor,
+    //             color,
+    //           }}
+    //         />
+    //       </>
+    //     );
+    //   },
+    // },
+
+    {
+      field: "actions",
+      headerName: "Actions",
+      cellRenderer: (params) => (
+        <div className="p-2">
+          <PrimaryButton
+            title={"View Details"}
+            handleSubmit={() => {
+              handleDetailsClick({ ...params.data });
+            }}
+          />
+        </div>
+      ),
     },
   ];
+
+  const handleDetailsClick = (asset) => {
+    setSelectedVisitor(asset);
+    setModalMode("view");
+    setIsModalOpen(true);
+  };
 
   const rows = [
     {
@@ -98,7 +125,6 @@ const VisitorReports = () => {
       status: "View Details",
     },
   ];
-  
 
   return (
     <div className="flex flex-col gap-8 p-4">
