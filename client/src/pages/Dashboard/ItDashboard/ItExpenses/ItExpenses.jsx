701a5ad00e8cbc77e58d7bacd6b6e61d5c805c7f
muskan-wono <muskan.wono@gmail.com>
2025-04-17 13:52:47 +0530

    Updated IT tables and added accordion to headers

--- client/src/pages/Dashboard/ItDashboard/ItExpenses/ItExpenses.jsx
+++ client/src/pages/Dashboard/ItDashboard/ItExpenses/ItExpenses.jsx
@@ -13,6 +13,8 @@ import { IoIosArrowDown } from "react-icons/io";
 import AgTable from "../../../../components/AgTable";
 import WidgetSection from "../../../../components/WidgetSection";
 import { useNavigate } from "react-router-dom";
+import dayjs from "dayjs";
+import { inrFormat } from "../../../../utils/currencyFormat";
 
 const ItExpenses = () => {
   const navigate = useNavigate();
@@ -398,7 +400,18 @@ const ItExpenses = () => {
       </WidgetSection>
 
       {/* Accordion Section for Domain-wise Revenue Breakdown */}
-      <div>
+      <div className="flex flex-col gap-2 border-default border-borderGray rounded-md p-4">
+      <div className="px-4 py-2 border-b-[1px] border-borderGray bg-gray-50">
+          <div className="flex justify-between items-center w-full px-4 py-2">
+            <span className="text-sm text-muted font-pmedium text-title">
+              LOCATION
+            </span>
+            <span className="text-sm text-muted font-pmedium text-title flex items-center gap-1">
+              REVENUE
+            </span>
+            
+          </div>
+        </div>
         {selectedMonthData.domains.map((domain, index) => {
           return (
             <Accordion key={index} className="py-4">
@@ -411,7 +424,7 @@ const ItExpenses = () => {
                     {domain.name}
                   </span>
                   <span className="text-subtitle font-pmedium">
-                    {domain.revenue.toLocaleString()}
+                    INR {domain.revenue.toLocaleString()}
                   </span>
                 </div>
               </AccordionSummary>
@@ -442,13 +455,14 @@ const ItExpenses = () => {
                 </div>
                 {/* Details End */}
                 <AgTable
-                  data={domain.clients}
+                  data={domain.clients.map((client,index)=>({...client,srNo:index+1,
+                                     registerDate:dayjs(client.registerDate).format("DD-MM-YYYY"),
+                                     actualRevenue:inrFormat(client.actualRevenue)
+                                   }))}
                   hideFilter
                   columns={[
                     {
-                      header: "Sr. No.",
-                      field: "client",
-                      flex: 1,
+                      headerName: "Sr No", field: "srNo", flex: 1 
                       // cellRenderer: (params) => (
                       //   <span
                       //     style={{
@@ -466,34 +480,46 @@ const ItExpenses = () => {
                       //   </span>
                       // ),
                     },
+                    // {
+                    //   headerName: "Unit No.",
+                    //   field: "representative",
+                    //   flex: 1,
+                    // },
+                    // { headerName: "Unit Floor", field: "registerDate", flex: 1 },
+                    // {
+                    //   headerName: "Unit Name",
+                    //   field: "actualRevenue",
+                    //   flex: 1,
+                    // },
+                    // {
+                    //   headerName: "Building",
+                    //   field: "actualRevenue",
+                    //   flex: 1,
+                    // },
+                    // {
+                    //   headerName: "Location",
+                    //   field: "actualRevenue",
+                    //   flex: 1,
+                    // },
+                    // {
+                    //   headerName: "Annual Expense",
+                    //   field: "actualRevenue",
+                    //   flex: 1,
+                    // },
+                    // {
+                    //   header: "Action",
+                    //   field: "actualRevenue",
+                    //   flex: 1,
+                    // },
+                    { headerName: "Client", field: "client", flex: 1 },
                     {
-                      header: "Unit No.",
+                      headerName: "Representative",
                       field: "representative",
                       flex: 1,
                     },
-                    { header: "Unit Floor", field: "registerDate", flex: 1 },
+                    { headerName: "Register Date", field: "registerDate", flex: 1 },
                     {
-                      header: "Unit Name",
-                      field: "actualRevenue",
-                      flex: 1,
-                    },
-                    {
-                      header: "Building",
-                      field: "actualRevenue",
-                      flex: 1,
-                    },
-                    {
-                      header: "Location",
-                      field: "actualRevenue",
-                      flex: 1,
-                    },
-                    {
-                      header: "Annual Expense",
-                      field: "actualRevenue",
-                      flex: 1,
-                    },
-                    {
-                      header: "Action",
+                      headerName: "Actual Revenue (INR)",
                       field: "actualRevenue",
                       flex: 1,
                     },
@@ -506,7 +532,7 @@ const ItExpenses = () => {
                       Total Revenue for {domain.name}:{" "}
                     </span>
                     <span className="text-black font-pmedium">
-                      ₹{domain.revenue.toLocaleString()}
+                      INR {domain.revenue.toLocaleString()}
                     </span>{" "}
                   </div>
                 </div>
