98f79271799b97677920130d06270fbd0274f99e
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2025-05-07 18:22:20 +0530

    Changes in budget

--- client/src/pages/Dashboard/HrDashboard/Finance/HrBudget.jsx
+++ client/src/pages/Dashboard/HrDashboard/Finance/HrBudget.jsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from "react";
-import LayerBarGraph from "../../../../components/graphs/LayerBarGraph";
+import React, { Suspense, useEffect, useState } from "react";
 import WidgetSection from "../../../../components/WidgetSection";
 import {
   TextField,
   Select,
   MenuItem,
   FormControl,
-  CircularProgress,
   Skeleton,
+  Box,
 } from "@mui/material";
 import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -22,9 +21,14 @@ import AllocatedBudget from "../../../../components/Tables/AllocatedBudget";
 import { toast } from "sonner";
 import BudgetGraph from "../../../../components/graphs/BudgetGraph";
 import { inrFormat } from "../../../../utils/currencyFormat";
+import { useNavigate } from "react-router-dom";
+import BarGraph from "../../../../components/graphs/BarGraph";
+import { transformBudgetData } from "../../../../utils/transformBudgetData";
 
 const HrBudget = () => {
   const axios = useAxiosPrivate();
+  const [isReady, setIsReady] = useState(false);
+
   const [openModal, setOpenModal] = useState(false);
   const { data: hrFinance = [], isPending: isHrLoading } = useQuery({
     queryKey: ["hrFinance"],
@@ -40,6 +44,128 @@ const HrBudget = () => {
       }
     },
   });
+
+   const budgetBar =  transformBudgetData(isHrLoading ? [] : hrFinance);
+  useEffect(() => {
+    if (!isHrLoading) {
+      const timer = setTimeout(() => setIsReady(true), 1000);
+      return () => clearTimeout(timer); // Cleanup on unmount
+    }
+  }, [isHrLoading]);
+
+  const expenseRawSeries = [
+    {
+      name: "FY 2024-25",
+      data: budgetBar?.utilisedBudget,
+      group: "total",
+    },
+    {
+      name: "FY 2025-26",
+      data: [1000054, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      group: "total",
+    },
+  ];
+
+  const expenseOptions = {
+    chart: {
+      type: "bar",
+      toolbar: { show: false },
+
+      stacked: true,
+      fontFamily: "Poppins-Regular, Arial, sans-serif",
+      events: {
+        dataPointSelection: () => {
+          navigate("finance/budget");
+        },
+      },
+    },
+    colors: ["#54C4A7", "#EB5C45"],
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "40%",
+        borderRadius: 5,
+        borderRadiusApplication: "none",
+        dataLabels: {
+          position: "top",
+        },
+      },
+    },
+    dataLabels: {
+      enabled: true,
+      formatter: (val) => {
+        const scaled = Math.round((val / 100000) * 100) / 100;
+        return Number.isInteger(scaled) ? scaled.toFixed(0) : scaled.toFixed(2);
+      },
+
+      style: {
+        fontSize: "12px",
+        colors: ["#000"],
+      },
+      offsetY: -22,
+    },
+    xaxis: {
+      categories: [
+        "Apr-24",
+        "May-24",
+        "Jun-24",
+        "Jul-24",
+        "Aug-24",
+        "Sep-24",
+        "Oct-24",
+        "Nov-24",
+        "Dec-24",
+        "Jan-25",
+        "Feb-25",
+        "Mar-25",
+      ],
+      title: {
+        text: "  ",
+      },
+    },
+    yaxis: {
+      // max: 3000000,
+      title: { text: "Amount In Lakhs (INR)" },
+      labels: {
+        formatter: (val) => `${Math.round(val / 100000)}`,
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+    legend: {
+      show: true,
+      position: "top",
+    },
+
+    tooltip: {
+      enabled: true,
+      custom: function ({ series, seriesIndex, dataPointIndex }) {
+        const rawData = expenseRawSeries[seriesIndex]?.data[dataPointIndex];
+        // return `<div style="padding: 8px; font-family: Poppins, sans-serif;">
+        //       HR Expense: INR ${rawData.toLocaleString("en-IN")}
+        //     </div>`;
+        return `
+            <div style="padding: 8px; font-size: 13px; font-family: Poppins, sans-serif">
+        
+              <div style="display: flex; align-items: center; justify-content: space-between; background-color: #d7fff4; color: #00936c; padding: 6px 8px; border-radius: 4px; margin-bottom: 4px;">
+                <div><strong>HR Expense:</strong></div>
+                <div style="width: 10px;"></div>
+             <div style="text-align: left;">INR ${Math.round(
+               rawData
+             ).toLocaleString("en-IN")}</div>
+
+              </div>
+     
+            </div>
+          `;
+      },
+    },
+  };
+
+  const totalUtilised =
+    hrFinance?.utilisedBudget?.reduce((acc, val) => acc + val, 0) || 0;
+  const navigate = useNavigate();
 
   const { control, handleSubmit, reset } = useForm({
     defaultValues: {
@@ -57,9 +183,8 @@ const HrBudget = () => {
   };
 
   // Transform data into the required format
-  const groupedData = isHrLoading
-    ? []
-    : hrFinance?.reduce((acc, item) => {
+  const groupedData = Array.isArray(hrFinance)
+    ? hrFinance?.reduce((acc, item) => {
         const month = dayjs(item.dueDate).format("MMM-YYYY"); // Extracting month and year
 
         if (!acc[month]) {
@@ -72,9 +197,9 @@ const HrBudget = () => {
               rows: [],
               columns: [
                 { field: "expanseName", headerName: "Expense Name", flex: 1 },
-                // { field: "department", headerName: "Department", flex: 200 },
                 { field: "expanseType", headerName: "Expense Type", flex: 1 },
-                { field: "projectedAmount", headerName: "Amount", flex: 1 },
+                { field: "projectedAmount", headerName: "Projected", flex: 1 },
+                { field: "actualAmount", headerName: "Actual", flex: 1 }, // ✅ add this
                 { field: "dueDate", headerName: "Due Date", flex: 1 },
                 { field: "status", headerName: "Status", flex: 1 },
               ],
@@ -89,13 +214,15 @@ const HrBudget = () => {
           expanseName: item?.expanseName,
           department: item?.department,
           expanseType: item?.expanseType,
-          projectedAmount: Number(item?.projectedAmount).toFixed(2), // Ensuring two decimal places
+          projectedAmount: Number(item?.projectedAmount).toFixed(2),
+          actualAmount: Number(item?.actualAmount || 0).toFixed(2), // ✅ Add this
           dueDate: dayjs(item.dueDate).format("DD-MM-YYYY"),
           status: item.status,
         });
 
         return acc;
-      }, {});
+      }, {})
+    : [];
 
   // Convert grouped data to array and sort by latest month (descending order)
   const financialData = Object.values(groupedData)
@@ -115,7 +242,8 @@ const HrBudget = () => {
       return {
         ...data,
         projectedAmount: data.projectedAmount.toLocaleString("en-IN"), // Ensuring two decimal places for total amount
-        amount: data.actualAmount.toLocaleString("en-IN"), // Ensuring two decimal places for total amount
+        amount: Number(data?.amount || 0).toLocaleString("en-IN"),
+        expanseType: data?.expanseType,
         tableData: {
           ...data.tableData,
           rows: transoformedRows,
@@ -126,184 +254,199 @@ const HrBudget = () => {
     .sort((a, b) => dayjs(b.latestDueDate).diff(dayjs(a.latestDueDate))); // Sort descending
 
   // ---------------------------------------------------------------------//
-  // Data for the chart
-  const utilisedData = [
-    125000, 150000, 99000, 85000, 70000, 50000, 80000, 95000, 100000, 65000,
-    50000, 120000,
-  ];
+  if (!isReady) {
+    return <Skeleton height="100vh" width="100%" />;
+  } else {
+    return (
+      <div className="flex flex-col gap-8">
+        <div>
+          <Suspense
+            fallback={
+              <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                {/* Simulating chart skeleton */}
+                <Skeleton variant="text" width={200} height={30} />
+                <Skeleton variant="rectangular" width="100%" height={300} />
+              </Box>
+            }
+          >
+            <WidgetSection
+              normalCase
+              layout={1}
+              border
+              padding
+              titleLabel={"FY 2024-25"}
+              // TitleAmount={"INR 23,13,365"}
+              TitleAmount={`INR ${Math.round(totalUtilised).toLocaleString(
+                "en-IN"
+              )}`}
+              title={"BIZ Nest HR DEPARTMENT EXPENSE"}
+            >
 
-  const maxBudget = [
-    100000, 120000, 100000, 100000, 80000, 60000, 85000, 95000, 100000, 70000,
-    60000, 110000,
-  ];
+              <BarGraph
+                data={expenseRawSeries}
+                options={expenseOptions}
+                departments={["FY 2024-25", "FY 2025-26"]}
+              />
+            </WidgetSection>
+          </Suspense>
+          ,
+        </div>
+        <div>
+          <WidgetSection layout={3} padding>
+            <DataCard
+              data={"INR " + inrFormat("2000000")}
+              title={"Projected"}
+              route={"/app/dashboard/hr-dashboard/finance/budget"}
+              description={`Current Month: ${new Date().toLocaleString(
+                "default",
+                {
+                  month: "short",
+                }
+              )}-25`}
+            />
+            <DataCard
+              data={"INR " + inrFormat("150000")}
+              title={"Actual"}
+              route={"/app/dashboard/hr-dashboard/finance/budget"}
+              description={`Current Month: ${new Date().toLocaleString(
+                "default",
+                {
+                  month: "short",
+                }
+              )}-25`}
+            />
+            <DataCard
+              data={"INR " + inrFormat(12000)}
+              title={"Requested"}
+              route={"/app/dashboard/hr-dashboard/finance/budget"}
+              description={`Current Month: ${new Date().toLocaleString(
+                "default",
+                {
+                  month: "short",
+                }
+              )}-25`}
+            />
+          </WidgetSection>
+        </div>
 
-  return (
-    <div className="flex flex-col gap-8">
-      <div>
-        <WidgetSection
-          layout={1}
-          title={"Budget v/s Achievements"}
-          titleLabel={"FY 2024-25"}
-          border
+        <div className="flex justify-end">
+          <PrimaryButton
+            title={"Request Budget"}
+            padding="px-5 py-2"
+            fontSize="text-base"
+            handleSubmit={() => setOpenModal(true)}
+          />
+        </div>
+
+        {!isHrLoading ? (
+          <AllocatedBudget
+            financialData={financialData}
+            isLoading={isHrLoading}
+          />
+        ) : (
+          <Skeleton height={600} width={"100%"} />
+        )}
+
+        <MuiModal
+          title="Request Budget"
+          open={openModal}
+          onClose={() => setOpenModal(false)}
         >
-          <BudgetGraph utilisedData={utilisedData} maxBudget={maxBudget} />
-        </WidgetSection>
-      </div>
-      <div>
-        <WidgetSection layout={3} padding>
-          <DataCard
-            data={"INR " + inrFormat("2000000")}
-            title={"Projected"}
-            route={"/app/dashboard/hr-dashboard/finance/budget"}
-            description={`Current Month: ${new Date().toLocaleString(
-              "default",
-              {
-                month: "short",
-              }
-            )}-25`}
-          />
-          <DataCard
-            data={"INR " + inrFormat("150000")}
-            title={"Actual"}
-            route={"/app/dashboard/hr-dashboard/finance/budget"}
-            description={`Current Month: ${new Date().toLocaleString(
-              "default",
-              {
-                month: "short",
-              }
-            )}-25`}
-          />
-          <DataCard
-            data={"INR " + inrFormat(12000)}
-            title={"Requested"}
-            route={"/app/dashboard/hr-dashboard/finance/budget"}
-            description={`Current Month: ${new Date().toLocaleString(
-              "default",
-              {
-                month: "short",
-              }
-            )}-25`}
-          />
-        </WidgetSection>
-      </div>
-
-      <div className="flex justify-end">
-        <PrimaryButton
-          title={"Request Budget"}
-          padding="px-5 py-2"
-          fontSize="text-base"
-          handleSubmit={() => setOpenModal(true)}
-        />
-      </div>
-
-      {!isHrLoading ? (
-        <AllocatedBudget
-          financialData={financialData}
-          isLoading={isHrLoading}
-        />
-      ) : (
-        <Skeleton height={600} width={"100%"} />
-      )}
-
-      <MuiModal
-        title="Request Budget"
-        open={openModal}
-        onClose={() => setOpenModal(false)}
-      >
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          {/* Expense Name */}
-          <Controller
-            name="expanseName"
-            control={control}
-            rules={{ required: "Expense name is required" }}
-            render={({ field, fieldState }) => (
-              <TextField
-                {...field}
-                label="Expense Name"
-                fullWidth
-                size="small"
-                error={!!fieldState.error}
-                helperText={fieldState.error?.message}
-              />
-            )}
-          />
-
-          {/* Expense Type */}
-          <Controller
-            name="expanseType"
-            control={control}
-            rules={{ required: "Expense type is required" }}
-            render={({ field, fieldState }) => (
-              <FormControl fullWidth error={!!fieldState.error}>
-                <Select {...field} size="small" displayEmpty>
-                  <MenuItem value="" disabled>
-                    Select Expense Type
-                  </MenuItem>
-                  <MenuItem value="Internal">Internal</MenuItem>
-                  <MenuItem value="External">External</MenuItem>
-                </Select>
-              </FormControl>
-            )}
-          />
-
-          {/* Amount */}
-          <Controller
-            name="amount"
-            control={control}
-            rules={{
-              required: "Amount is required",
-              pattern: {
-                value: /^[0-9]+(\.[0-9]{1,2})?$/,
-                message: "Enter a valid amount",
-              },
-            }}
-            render={({ field, fieldState }) => (
-              <TextField
-                {...field}
-                label="Amount"
-                fullWidth
-                size="small"
-                error={!!fieldState.error}
-                helperText={fieldState.error?.message}
-              />
-            )}
-          />
-
-          {/* Due Date */}
-          <Controller
-            name="dueDate"
-            control={control}
-            rules={{ required: "Due date is required" }}
-            render={({ field, fieldState }) => (
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            {/* Expense Name */}
+            <Controller
+              name="expanseName"
+              control={control}
+              rules={{ required: "Expense name is required" }}
+              render={({ field, fieldState }) => (
+                <TextField
                   {...field}
-                  label="Due Date"
-                  format="DD-MM-YYYY"
-                  value={field.value ? dayjs(field.value) : null}
-                  onChange={(date) =>
-                    field.onChange(date ? date.toISOString() : null)
-                  }
-                  slotProps={{
-                    textField: {
-                      fullWidth: true,
-                      size: "small",
-                      error: !!fieldState.error,
-                      helperText: fieldState.error?.message,
-                    },
-                  }}
+                  label="Expense Name"
+                  fullWidth
+                  size="small"
+                  error={!!fieldState.error}
+                  helperText={fieldState.error?.message}
                 />
-              </LocalizationProvider>
-            )}
-          />
-          <div className="flex justify-center items-center">
-            {/* Submit Button */}
-            <PrimaryButton type={"submit"} title={"Submit"} />
-          </div>
-        </form>
-      </MuiModal>
-    </div>
-  );
+              )}
+            />
+
+            {/* Expense Type */}
+            <Controller
+              name="expanseType"
+              control={control}
+              rules={{ required: "Expense type is required" }}
+              render={({ field, fieldState }) => (
+                <FormControl fullWidth error={!!fieldState.error}>
+                  <Select {...field} size="small" displayEmpty>
+                    <MenuItem value="" disabled>
+                      Select Expense Type
+                    </MenuItem>
+                    <MenuItem value="Internal">Internal</MenuItem>
+                    <MenuItem value="External">External</MenuItem>
+                  </Select>
+                </FormControl>
+              )}
+            />
+
+            {/* Amount */}
+            <Controller
+              name="amount"
+              control={control}
+              rules={{
+                required: "Amount is required",
+                pattern: {
+                  value: /^[0-9]+(\.[0-9]{1,2})?$/,
+                  message: "Enter a valid amount",
+                },
+              }}
+              render={({ field, fieldState }) => (
+                <TextField
+                  {...field}
+                  label="Amount"
+                  fullWidth
+                  size="small"
+                  error={!!fieldState.error}
+                  helperText={fieldState.error?.message}
+                />
+              )}
+            />
+
+            {/* Due Date */}
+            <Controller
+              name="dueDate"
+              control={control}
+              rules={{ required: "Due date is required" }}
+              render={({ field, fieldState }) => (
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    {...field}
+                    label="Due Date"
+                    format="DD-MM-YYYY"
+                    value={field.value ? dayjs(field.value) : null}
+                    onChange={(date) =>
+                      field.onChange(date ? date.toISOString() : null)
+                    }
+                    slotProps={{
+                      textField: {
+                        fullWidth: true,
+                        size: "small",
+                        error: !!fieldState.error,
+                        helperText: fieldState.error?.message,
+                      },
+                    }}
+                  />
+                </LocalizationProvider>
+              )}
+            />
+            <div className="flex justify-center items-center">
+              {/* Submit Button */}
+              <PrimaryButton type={"submit"} title={"Submit"} />
+            </div>
+          </form>
+        </MuiModal>
+      </div>
+    );
+  }
 };
 
 export default HrBudget;
