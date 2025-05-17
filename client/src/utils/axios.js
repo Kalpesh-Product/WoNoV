dba4c72e8d02d5022280fcf4aae0a2cbca9422c9
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2025-05-17 13:25:49 +0530

    integrated sales data

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_DEV_LINK,
-});
-
-export const axiosPrivate = axios.create({
-  baseURL: import.meta.env.VITE_DEV_LINK,
-  withCredentials: true,
-});
-
 // export const api = axios.create({
-//   baseURL: import.meta.env.VITE_PROD_LINK,
+//   baseURL: import.meta.env.VITE_DEV_LINK,
 // });
 
 // export const axiosPrivate = axios.create({
-//   baseURL: import.meta.env.VITE_PROD_LINK,
+//   baseURL: import.meta.env.VITE_DEV_LINK,
 //   withCredentials: true,
 // });
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_PROD_LINK,
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: import.meta.env.VITE_PROD_LINK,
+  withCredentials: true,
+});
