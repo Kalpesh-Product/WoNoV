04152825ba6ff182b260412328f1a7a75994f8a0
sankalp-wono <sankalp.wono@gmail.com>
2025-05-28 17:27:45 +0530

    some more refinements

--- server/controllers/meetingsControllers/meetingsControllers.js
+++ server/controllers/meetingsControllers/meetingsControllers.js
@@ -388,12 +388,14 @@ const getMeetings = async (req, res, next) => {
           },
         },
       })
-      .populate("bookedBy", "firstName lastName")
-      .populate("receptionist", "firstName lastName")
-      .populate("client", "clientName")
-      .populate("internalParticipants", "firstName lastName email")
-      .populate("clientParticipants", "employeeName email")
-      .populate("externalParticipants", "firstName lastName email");
+      .populate([
+        { path: "bookedBy", select: "firstName lastName" },
+        { path: "receptionist", select: "firstName lastName" },
+        { path: "client", select: "clientName" },
+        { path: "internalParticipants", select: "firstName lastName email" },
+        { path: "clientParticipants", select: "employeeName email" },
+        { path: "externalParticipants", select: "firstName lastName email" },
+      ]);
 
     const departments = await User.findById({ _id: user }).select(
       "departments"
