d97527450dcdaa8f1569fdc07d53daaa54126632
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2025-05-31 12:13:54 +0530

    Calendar changes

--- client/src/pages/Meetings/Calendar.jsx
+++ client/src/pages/Meetings/Calendar.jsx
@@ -28,6 +28,7 @@ const Calender = () => {
   const [selectedEvent, setSelectedEvent] = useState(null);
   const [filteredEvents, setFilteredEvents] = useState([]);
   const [headerBackground, setHeaderBackground] = useState("");
+  const axios = useAxiosPrivate();
   const [newEvent, setNewEvent] = useState({
     title: "",
     start: "",
@@ -38,29 +39,40 @@ const Calender = () => {
     "completed", // ⬅ change "Completed" to "completed"
   ]);
 
-  const meetings = useSelector((state) => state.meetings.data);
-  console.log("Calendar Meetings from redux", meetings);
-
-  const transformedMeetings = meetings.map((meeting) => {
-    const formattedStart = meeting.startTime.split("/").reverse().join("-");
-    const formattedEnd = meeting.endTime.split("/").reverse().join("-");
-
-    const status = meeting.meetingStatus.toLowerCase();
-    const colors = {
-      upcoming: "#3BACFF",
-      completed: "#5EFE1F",
-    };
-
-    return {
-      id: meeting._id,
-      title: meeting.subject,
-      start: formattedStart,
-      end: formattedEnd,
-      backgroundColor: colors[status] || undefined, // ✅ Add background color here
-      borderColor: colors[status] || undefined,
-      extendedProps: { ...meeting },
-    };
+  const { data: meetings = [], isLoading: isMeetingsLoading } = useQuery({
+    queryKey: ["meetings-calendar"],
+    queryFn: async () => {
+      try {
+        const respone = await axios.get("/api/meetings/my-meetings");
+        return respone.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
   });
+
+  const transformedMeetings = isMeetingsLoading
+    ? []
+    : meetings.map((meeting) => {
+        const formattedStart = meeting.startTime.split("/").reverse().join("-");
+        const formattedEnd = meeting.endTime.split("/").reverse().join("-");
+
+        const status = meeting.meetingStatus.toLowerCase();
+        const colors = {
+          upcoming: "#3BACFF",
+          completed: "#5EFE1F",
+        };
+
+        return {
+          id: meeting._id,
+          title: meeting.subject,
+          start: formattedStart,
+          end: formattedEnd,
+          backgroundColor: colors[status] || undefined, // ✅ Add background color here
+          borderColor: colors[status] || undefined,
+          extendedProps: { ...meeting },
+        };
+      });
 
   console.log("Calendar Meetings from redux transformed", transformedMeetings);
 
@@ -120,126 +132,132 @@ const Calender = () => {
   return (
     <div className="flex w-[70%] md:w-full">
       <div className="flex-1 p-4 bg-white">
-        <>
-          <div className="flex gap-4 relative w-full">
-            <div className="flex flex-col gap-4 w-[25%]">
-              <div className="border-2 border-gray-300  rounded-md">
-                <div className="w-full flex justify-start border-b-default border-borderGray p-2">
-                  <span className="text-content font-bold uppercase">
-                    Event Filters
-                  </span>
-                </div>
-                <div className="flex justify-start text-content px-2">
-                  <FormGroup column>
-                    {["upcoming", "Completed"].map((type) => {
-                      const normalizedType = type.toLowerCase(); // ✅ always lowercase for logic
-                      const colors = {
-                        upcoming: "#3BACFF",
-                        completed: "#5EFE1F",
-                      };
-                      return (
-                        <FormControlLabel
-                          key={type}
-                          control={
-                            <Checkbox
-                              sx={{
-                                fontSize: "0.75rem",
-                                transform: "scale(0.8)",
-                                color: colors[normalizedType],
-                                "&.Mui-checked": {
+        {!isMeetingsLoading ? (
+          <>
+            <div className="flex gap-4 relative w-full">
+              <div className="flex flex-col gap-4 w-[25%]">
+                <div className="border-2 border-gray-300  rounded-md">
+                  <div className="w-full flex justify-start border-b-default border-borderGray p-2">
+                    <span className="text-content font-bold uppercase">
+                      Event Filters
+                    </span>
+                  </div>
+                  <div className="flex justify-start text-content px-2">
+                    <FormGroup column>
+                      {["upcoming", "Completed"].map((type) => {
+                        const normalizedType = type.toLowerCase(); // ✅ always lowercase for logic
+                        const colors = {
+                          upcoming: "#3BACFF",
+                          completed: "#5EFE1F",
+                        };
+                        return (
+                          <FormControlLabel
+                            key={type}
+                            control={
+                              <Checkbox
+                                sx={{
+                                  fontSize: "0.75rem",
+                                  transform: "scale(0.8)",
                                   color: colors[normalizedType],
-                                },
-                              }}
-                              checked={eventFilter.includes(normalizedType)}
-                              onChange={(e) => {
-                                const selectedType = normalizedType;
-                                setEventFilter((prevFilter) =>
-                                  e.target.checked
-                                    ? [...prevFilter, selectedType]
-                                    : prevFilter.filter(
-                                        (t) => t !== selectedType
-                                      )
-                                );
-                              }}
-                              value={normalizedType}
-                            />
-                          }
-                          label={
-                            <span style={{ fontSize: "0.875rem" }}>
-                              {type.charAt(0).toUpperCase() + type.slice(1)}
-                            </span>
-                          }
-                        />
-                      );
-                    })}
-                  </FormGroup>
-                </div>
-              </div>
-
-              <div className="border-2 border-gray-300  rounded-md">
-                <div className="mb-2 text-content font-bold uppercase border-b-default border-borderGray p-2">
-                  <span>Today's Schedule</span>
+                                  "&.Mui-checked": {
+                                    color: colors[normalizedType],
+                                  },
+                                }}
+                                checked={eventFilter.includes(normalizedType)}
+                                onChange={(e) => {
+                                  const selectedType = normalizedType;
+                                  setEventFilter((prevFilter) =>
+                                    e.target.checked
+                                      ? [...prevFilter, selectedType]
+                                      : prevFilter.filter(
+                                          (t) => t !== selectedType
+                                        )
+                                  );
+                                }}
+                                value={normalizedType}
+                              />
+                            }
+                            label={
+                              <span style={{ fontSize: "0.875rem" }}>
+                                {type.charAt(0).toUpperCase() + type.slice(1)}
+                              </span>
+                            }
+                          />
+                        );
+                      })}
+                    </FormGroup>
+                  </div>
                 </div>
 
-                <div className="px-2">
-                  {todaysEvents.length > 0 ? (
-                    todaysEvents.map((event, index) => {
-                      const colors = {
-                        Upcoming: "#3BACFF",
-                        Completed: "#5EFE1F",
-                      };
-                      return (
-                        <div
-                          key={index}
-                          className="flex gap-2 items-center mb-2"
-                        >
+                <div className="border-2 border-gray-300  rounded-md">
+                  <div className="mb-2 text-content font-bold uppercase border-b-default border-borderGray p-2">
+                    <span>Today's Schedule</span>
+                  </div>
+
+                  <div className="px-2">
+                    {todaysEvents.length > 0 ? (
+                      todaysEvents.map((event, index) => {
+                        const colors = {
+                          Upcoming: "#3BACFF",
+                          Completed: "#5EFE1F",
+                        };
+                        return (
                           <div
-                            className="w-3 h-3 rounded-full mr-2"
-                            style={{
-                              backgroundColor:
-                                colors[event.extendedProps.meetingStatus],
-                            }}
-                          ></div>
-                          <div className="flex flex-col">
-                            <span className="text-content font-medium">
-                              {event.title}
-                            </span>
-                            <span className="text-small text-gray-500">
-                              {event.start
-                                ? dayjs(event.start).format("h:mm A")
-                                : "All Day"}
-                            </span>
+                            key={index}
+                            className="flex gap-2 items-center mb-2"
+                          >
+                            <div
+                              className="w-3 h-3 rounded-full mr-2"
+                              style={{
+                                backgroundColor:
+                                  colors[event.extendedProps.meetingStatus],
+                              }}
+                            ></div>
+                            <div className="flex flex-col">
+                              <span className="text-content font-medium">
+                                {event.title}
+                              </span>
+                              <span className="text-small text-gray-500">
+                                {event.start
+                                  ? dayjs(event.start).format("h:mm A")
+                                  : "All Day"}
+                              </span>
+                            </div>
                           </div>
-                        </div>
-                      );
-                    })
-                  ) : (
-                    <span>No events today.</span>
-                  )}
+                        );
+                      })
+                    ) : (
+                      <span>No events today.</span>
+                    )}
+                  </div>
                 </div>
               </div>
+              <div className="w-full h-full overflow-y-auto">
+                <FullCalendar
+                  headerToolbar={{
+                    left: "today",
+                    center: "prev title next",
+                    right: "dayGridMonth,timeGridWeek,timeGridDay",
+                  }}
+                  dayMaxEvents={2}
+                  eventDisplay="block"
+                  eventContent={(meeting) => (
+                    <span className="text-content">{meeting.event.title}</span>
+                  )}
+                  eventClick={handleEventClick}
+                  contentHeight={520}
+                  plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                  initialView="dayGridMonth"
+                  events={filteredEvents}
+                />
+              </div>
             </div>
-            <div className="w-full h-full overflow-y-auto">
-              <FullCalendar
-                headerToolbar={{
-                  left: "today",
-                  center: "prev title next",
-                  right: "dayGridMonth,timeGridWeek,timeGridDay",
-                }}
-                dayMaxEvents={2}
-                eventDisplay="block"
-                eventContent={(meeting) => (
-                  <span className="text-content">{meeting.event.title}</span>
-                )}
-                eventClick={handleEventClick}
-                contentHeight={520}
-                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                initialView="dayGridMonth"
-                events={filteredEvents}
-              />
-            </div>
+          </>
+        ) : (
+          <div className="h-72 flex justify-center items-center">
+            <CircularProgress />
           </div>
-        </>
+        )}
 
         <MuiModal
           open={isDrawerOpen}
