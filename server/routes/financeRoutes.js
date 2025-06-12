b67405fed322654c9624f5a3aa38ab5eccbb2921
muskan-wono <muskan.wono@gmail.com>
2025-06-12 18:26:50 +0530

    Created upload and get invoice controllers

--- server/routes/financeRoutes.js
+++ server/routes/financeRoutes.js
@@ -1,5 +1,16 @@
-const getIncomeAndExpanse = require("../controllers/financeControllers/financeControllers");
 const router = require("express").Router();
+const {
+  getIncomeAndExpanse,
+  uploadClientInvoice,
+  getInvoices,
+} = require("../controllers/financeControllers/financeControllers");
+const upload = require("../config/multerConfig");
 
 router.get("/income-expense", getIncomeAndExpanse);
+router.get("/client-invoices", getInvoices);
+router.post(
+  "/upload-client-invoice",
+  upload.single("client-invoice"),
+  uploadClientInvoice
+);
 module.exports = router;
