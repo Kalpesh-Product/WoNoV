8a8bb292afea01258bd1ea2dbfc98b80038bbdca
sankalp-wono <sankalp.wono@gmail.com>
2025-06-21 11:32:18 +0530

    some changes

--- server/controllers/companyControllers/houseKeepingController.js
+++ server/controllers/companyControllers/houseKeepingController.js
@@ -1,5 +1,6 @@
 const HouseKeepingStaff = require("../../models/hr/HouseKeepingStaff");
 const Users = require("../../models/hr/UserData");
+const Department = require("../../models/Departments");
 
 const addNewHouseKeepingMember = async (req, res, next) => {
   try {
@@ -10,11 +11,18 @@ const addNewHouseKeepingMember = async (req, res, next) => {
         .json({ message: "Please provide the valid details" });
     }
 
+    const adminDepartment = await Department.findOne({
+      name: "6798bae6e469e809084e24a4",
+    })
+      .lean()
+      .exec();
+
     const newHouseKeepingStaff = new HouseKeepingStaff({
       name,
       gender,
       manager,
       unit,
+      department: adminDepartment._id,
     });
 
     const savedHouseKeepingStaff = await newHouseKeepingStaff.save();
@@ -33,6 +41,7 @@ const getHouseKeepingStaff = async (req, res, next) => {
       .populate([
         { path: "unit", select: "unitNo unitName" },
         { path: "manager", select: "roleTitle" },
+        { path: "department", select: "name" },
       ])
       .lean()
       .exec();
