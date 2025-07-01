9f557a91f137f08d96d973db44c485a8d5a2bffe
sankalp-wono <sankalp.wono@gmail.com>
2025-07-01 14:03:49 +0530

    some changes

--- server/controllers/inventoryControllers/inventoryControllers.js
+++ server/controllers/inventoryControllers/inventoryControllers.js
@@ -2,6 +2,8 @@ const { default: mongoose } = require("mongoose");
 const Inventory = require("../../models/inventory/Inventory");
 const CustomError = require("../../utils/customErrorlogs");
 const { createLog } = require("../../utils/moduleLogs");
+const { Readable } = require("stream");
+const csvParser = require("csv-parser");
 
 // Create Inventory Item
 const createInventory = async (req, res, next) => {
@@ -129,8 +131,68 @@ const updateInventory = async (req, res) => {
   }
 };
 
+const bulkInsertInventory = async (req, res, next) => {
+  try {
+    const { departmentId } = req.params;
+    const companyId = req.company; 
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).json({ message: "Please provide a CSV file" });
+    }
+
+    const csv = file.buffer.toString("utf8").trim();
+    const results = [];
+
+    Readable.from(csv)
+      .pipe(csvParser())
+      .on("data", (row) => {
+        const mappedRow = {
+          company: companyId,
+          department: departmentId,
+          itemName: row["itemName"],
+          openingInventoryUnits: Number(row["Opening Inventory Units"]),
+          openingPerUnitPrice: Number(row["Opening Per Unit Price"]),
+          openingInventoryValue: Number(row["Opening Inventory Value"]),
+          newPurchaseUnits: Number(row["New Purchase Units"] || 0),
+          newPurchasePerUnitPrice: Number(
+            row["New purchase per unit price"] || 0
+          ),
+          newPurchaseInventoryValue: Number(
+            row["New purchase inventory value"] || 0
+          ),
+          closingInventoryUnits: Number(row["Closing inventory units"] || 0),
+          category: row["Category"],
+        };
+        results.push(mappedRow);
+      })
+      .on("end", async () => {
+        if (results.length === 0) {
+          return res
+            .status(400)
+            .json({ message: "CSV file is empty or invalid" });
+        }
+
+        await Inventory.insertMany(results);
+        res.status(200).json({
+          message: "Inventory uploaded successfully",
+          count: results.length,
+        });
+      })
+      .on("error", (error) => {
+        console.error("CSV Parsing Error:", error);
+        res
+          .status(500)
+          .json({ message: "Failed to parse CSV", error: error.message });
+      });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   createInventory,
   getInventories,
   updateInventory,
+  bulkInsertInventory,
 };
