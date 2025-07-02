6ed34c19c801b308b1220160b82175537dbd3a9c
muskan-wono <muskan.wono@gmail.com>
2025-07-02 18:22:22 +0530

    Integrated SOPs and policies

--- server/controllers/companyControllers/documentControllers.js
+++ server/controllers/companyControllers/documentControllers.js
@@ -223,6 +223,7 @@ const getCompanyDocuments = async (req, res, next) => {
       return res.status(404).json({ message: "Company not found" });
     }
 
+    console.log("sops");
     return res.status(200).json({ [type]: foundUser.company[type] || [] });
   } catch (error) {
     next(error);
