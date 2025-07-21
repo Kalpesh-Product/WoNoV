9ae4077e272ef9c8b943b71478487930a36834f5
muskan-wono <muskan.wono@gmail.com>
2025-07-21 15:37:50 +0530

    typo

--- server/models/assets/AssetSubCategories.js
+++ server/models/assets/AssetSubCategories.js
@@ -1,21 +1,26 @@
 const mongoose = require("mongoose");
-const subCategorySchema = new mongoose.Schema({
-  subCategoryName: {
-    type: String,
-    required: true,
+const subCategorySchema = new mongoose.Schema(
+  {
+    subCategoryName: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "AssetCategory",
+    },
+    description: {
+      type: String,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
   },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "AssetCategory",
-  },
-  description: {
-    type: String,
-  },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const AssetSubCategory = mongoose.model("SubCategory", subCategorySchema);
 module.exports = AssetSubCategory;
