8d6ce1e5d0136096ec78c8fe54c38a50e7a70ed9
sankalp-wono <sankalp.wono@gmail.com>
2025-07-24 18:02:41 +0530

    created bulk insert routes and a new fetch employee routes

--- server/routes/accessRoutes.js
+++ server/routes/accessRoutes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const {
   getPermissions,
-  updatePermissions
+  updatePermissions,
+  getDepartmentWiseUsers,
 } = require("../controllers/accessController/accessController");
 
 router.get("/user-permissions/:userId", getPermissions);
 router.post("/modify-permissions/:userId", updatePermissions);
+router.get("/department-wise-employees", getDepartmentWiseUsers);
 
 module.exports = router;
--- server/routes/administrationRoutes.js
+++ server/routes/administrationRoutes.js
@@ -1,23 +1,36 @@
 const router = require("express").Router();
+const upload = require("../config/multerConfig");
 
 const {
   assignWeeklyUnit,
   updateWeeklyUnit,
   fetchWeeklyUnits,
   addSubstitute,
+  bulkInsertWeeklyShiftSchedule,
 } = require("../controllers/weeklyScheduleControllers/WeeklyScheduleController");
 const {
   createAdminEvent,
   getAdminEvents,
   updateAdminEvent,
+  bulkInsertClientEvents,
 } = require("../controllers/adminControllers/adminEventsControllers");
 
 router.post("/assign-weekly-unit", assignWeeklyUnit);
 router.patch("/update-weekly-unit", updateWeeklyUnit);
 router.patch("/add-substitute", addSubstitute);
+router.post(
+  "/bulk-insert-weekly-shift-schedule",
+  upload.single("schedule"),
+  bulkInsertWeeklyShiftSchedule
+);
 router.get("/fetch-weekly-unit/:department", fetchWeeklyUnits);
 router.get("/events/:id", getAdminEvents);
 router.post("/create-admin-event", createAdminEvent);
 router.patch("/update-admin-event", updateAdminEvent);
+router.post(
+  "/bulk-insert-client-events",
+  upload.single("client-events"),
+  bulkInsertClientEvents
+);
 
 module.exports = router;
