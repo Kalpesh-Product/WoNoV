716db77337a0371022647965ef50499945a9320a
allanwono <allan.wono@gmail.com>
2025-12-31 18:39:45 +0530

    review star colors yellow

--- client/src/components/CustomRating.jsx
+++ client/src/components/CustomRating.jsx
@@ -4,7 +4,7 @@ import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied
 import SentimentSatisfiedIcon from "@mui/icons-material/SentimentSatisfied";
 import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAltOutlined";
 import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
-import StarIcon from '@mui/icons-material/Star';
+import StarIcon from "@mui/icons-material/Star";
 
 const StyledRating = styled(Rating)(({ theme }) => ({
   "& .MuiRating-iconEmpty .MuiSvgIcon-root": {
@@ -17,12 +17,12 @@ const customIcons = {
   // 2: { icon: <SentimentDissatisfiedIcon color="error" />, label: "Dissatisfied" },
   // 3: { icon: <SentimentSatisfiedIcon color="warning" />, label: "Neutral" },
   // 4: { icon: <SentimentSatisfiedAltIcon color="success" />, label: "Satisfied" },
-  // 5: { icon: <SentimentVerySatisfiedIcon color="success" />, label: "Very Satisfied" }, 
-  1: { icon: <StarIcon color="error" />, label: "Very Dissatisfied" },
-  2: { icon: <StarIcon color="error" />, label: "Dissatisfied" },
-  3: { icon: <StarIcon color="warning" />, label: "Neutral" },
-  4: { icon: <StarIcon color="success" />, label: "Satisfied" },
-  5: { icon: <StarIcon color="success" />, label: "Very Satisfied" },
+  // 5: { icon: <SentimentVerySatisfiedIcon color="success" />, label: "Very Satisfied" },
+  1: { icon: <StarIcon color="#ffcd38" />, label: "Very Dissatisfied" },
+  2: { icon: <StarIcon color="#ffcd38" />, label: "Dissatisfied" },
+  3: { icon: <StarIcon color="#ffcd38" />, label: "Neutral" },
+  4: { icon: <StarIcon color="#ffcd38" />, label: "Satisfied" },
+  5: { icon: <StarIcon color="#ffcd38" />, label: "Very Satisfied" },
 };
 
 function IconContainer(props) {
@@ -35,8 +35,8 @@ const CustomRating = (props) => (
     {...props}
     IconContainerComponent={IconContainer}
     getLabelText={(value) => customIcons[value]?.label || ""}
-    highlightSelectedOnly
-        sx={{
+    // highlightSelectedOnly
+    sx={{
       "& .MuiSvgIcon-root": {
         fontSize: 64, // adjust this value to your preferred size
       },
