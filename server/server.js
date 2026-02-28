ba3f82cdb1976c4e8c323666122b9182477c94d6
muskan-wono <muskan.wono@gmail.com>
2026-02-28 17:42:48 +0530

    chore: comment generate hashed password function

--- server/server.js
+++ server/server.js
@@ -131,7 +131,7 @@ mongoose.connection.once("open", () => {
 
 // For Generating hashed password for testing
 // (async () => {
-//   const hashed = await hashPassword("Pass@1234");
+//   const hashed = await hashPassword("Valentina@2435");
 //   console.log(hashed);
 // })();
 
