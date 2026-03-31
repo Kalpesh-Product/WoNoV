416386dadfe1fac47a36e56f15382ae1e3577150
pranav-wono <pranavv.wono@gmail.com>
2026-03-31 15:35:00 +0530

    feat: implement ListOfAssets component for managing and editing asset records

--- client/src/pages/Assets/AssetsCategory/ListOfAssets.jsx
+++ client/src/pages/Assets/AssetsCategory/ListOfAssets.jsx
@@ -522,7 +522,7 @@ const ListOfAssets = () => {
                     ? []
                     : vendorDetails.map((item) => (
                       <MenuItem key={item._id} value={item._id} disabled={!item.isActive}>
-                        {item.companyName || item.name}
+                        {item.isActive ? (item.companyName || item.name) : (item.companyName || item.name) + "(Inactive)"}
                       </MenuItem>
                     ))}
                 </TextField>
