46aff638a1093779e5a9fcf49dafd99e1dbbaaad
muskan-wono <muskan.wono@gmail.com>
2026-04-25 15:25:38 +0530

    feat: add enum to status

--- server/models/sales/WorkationRevenue.js
+++ server/models/sales/WorkationRevenue.js
@@ -24,6 +24,8 @@ const workationRevenueSchema = new mongoose.Schema(
     },
     status: {
       type: String,
+      enum: ["Paid", "UnPaid", "Partially Paid"],
+      default: "UnPaid",
     },
     gst: {
       type: Number,
