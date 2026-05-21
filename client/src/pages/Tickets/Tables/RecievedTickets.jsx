c3effa6aa5c5d1d6313e63c994b1025f20633fbf
vishal-wono <vishal.wono@gmail.com>
2026-05-21 12:36:54 +0530

    Fixed Tickets Problem Header

--- client/src/pages/Tickets/Tables/RecievedTickets.jsx
+++ client/src/pages/Tickets/Tables/RecievedTickets.jsx
@@ -295,7 +295,10 @@ const RecievedTickets = ({ title, departmentId }) => {
   return (
     <div className="p-4 border-default border-borderGray rounded-md">
       <div className="pb-4">
-        <span className="text-subtitle">{title}</span>
+        {/* <span className="text-subtitle">{title}</span> */}
+        <span className="text-mobileTitle lg:text-widgetTitle text-primary font-pmedium uppercase">
+          {title}
+        </span>
       </div>
       <div className="w-full">
         {isLoading ? (
