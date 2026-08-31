67856b3222f4a47a55724e429cc17ab16eb7ebee
vishal-wono <vishal.wono@gmail.com>
2026-08-31 18:44:56 +0530

    feat: remove unused budget and performance routes from bulkInsertRoutes

--- client/src/constants/bulkInsertRoutes.js
+++ client/src/constants/bulkInsertRoutes.js
@@ -7,42 +7,41 @@ const bulkInsertRoutes = [
         fileKey: "assets",
         route: "/api/assets/bulk-insert-assets/6798bae6e469e809084e24a4",
       },
-      {
-        name: "budget",
-        fileKey: "budgets",
-        route: "/api/budget/bulk-insert-budget/6798bae6e469e809084e24a4",
-      },
-      {
-        name: "performance",
-         // aliases: ["KRA", "KPA", "KRA KPA", "KRA/KPA", "performance tasks"],
-        fileKey: "performance-tasks",
-        route:
-          "/api/performance/bulk-insert-performance-tasks/6798bae6e469e809084e24a4",
-      },
+      // {
+      //   name: "budget",
+      //   fileKey: "budgets",
+      //   route: "/api/budget/bulk-insert-budget/6798bae6e469e809084e24a4",
+      // },
+      // {
+      //   name: "performance",
+      //   fileKey: "performance-tasks",
+      //   route:
+      //     "/api/performance/bulk-insert-performance-tasks/6798bae6e469e809084e24a4",
+      // },
       {
         name: "vendors",
         fileKey: "vendors",
         route: "/api/vendors/bulk-insert-vendors/6798bae6e469e809084e24a4",
       },
-      {
-        name: "weekly shift",
-        fileKey: "schedule",
-        route: "/api/administration/bulk-insert-weekly-shift-schedule",
-      },
+      // {
+      //   name: "weekly shift",
+      //   fileKey: "schedule",
+      //   route: "/api/administration/bulk-insert-weekly-shift-schedule",
+      // },
       {
         name: "client events",
         fileKey: "client-events",
         route: "/api/administration/bulk-insert-client-events",
       },
-      {
-        name: "co-working members",
-        fileKey: "members",
-        route: "/api/sales/bulk-insert-co-working-client-members",
-      },
-      {
-        name: "house-keeping schedule",
-        route: "/api/company/bulk-insert-housekeeping-schedule",
-      },
+      // {
+      //   name: "co-working members",
+      //   fileKey: "members",
+      //   route: "/api/sales/bulk-insert-co-working-client-members",
+      // },
+      // {
+      //   name: "house-keeping schedule",
+      //   route: "/api/company/bulk-insert-housekeeping-schedule",
+      // },
       {
         name: "inventory",
         route: "/api/invenotry/bulk-insert-inventory/6798bae6e469e809084e24a4",
@@ -182,28 +181,27 @@ const bulkInsertRoutes = [
         fileKey: "assets",
         route: "/api/assets/bulk-insert-assets/6798bacce469e809084e24a1",
       },
-      {
-        name: "budget", //working
-        fileKey: "budgets",
-        route: "/api/budget/bulk-insert-budget/6798bacce469e809084e24a1",
-      },
-      {
-        name: "performance", //working
-         //aliases: ["KRA", "KPA", "KRA KPA", "KRA/KPA", "performance tasks"],
-        fileKey: "performance-tasks",
-        route:
-          "/api/performance/bulk-insert-performance-tasks/6798bacce469e809084e24a1",
-      },
+      // {
+      //   name: "budget", //working
+      //   fileKey: "budgets",
+      //   route: "/api/budget/bulk-insert-budget/6798bacce469e809084e24a1",
+      // },
+      // {
+      //   name: "performance", //working
+      //   fileKey: "performance-tasks",
+      //   route:
+      //     "/api/performance/bulk-insert-performance-tasks/6798bacce469e809084e24a1",
+      // },
       {
         name: "vendors",
         fileKey: "vendors",
         route: "/api/vendors/bulk-insert-vendors/6798bacce469e809084e24a1",
       },
-      {
-        name: "co-working members",
-        fileKey: "members",
-        route: "/api/sales/bulk-insert-co-working-client-members",
-      },
+      // {
+      //   name: "co-working members",
+      //   fileKey: "members",
+      //   route: "/api/sales/bulk-insert-co-working-client-members",
+      // },
       {
         name: "Virtual Office Clients",
         aliases: ["Virtual Office Client"],
